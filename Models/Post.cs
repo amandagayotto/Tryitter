59fb3b2c64dd3741373201b4bd8d0b230cc46c89
Amanda <amandagayotto@hotmail.com>
2022-12-11 19:40:37 -0300

    feat: adiciona Data Annotations

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace tryitter.Models
 {
     public class Post
     {
+        [Key]
         public int PostId { get; set; }
 
+        [Required]
+        [MaxLength(300)]
         public string? Texto { get; set; }
 
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
+        [Required]
         public int UserId { get; set; }
 
         public User? User { get; set; }
--- Models/User.cs
+++ Models/User.cs
@@ -1,17 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace tryitter.Models
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string? Nome { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string? Email { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string? Senha { get; set; }
 
+        [StringLength(80)]
         public string? Modulo { get; set; }
 
+        [StringLength(300)]
         public string? Status { get; set; }
 
         public ICollection<Post>? Posts { get; set; }
