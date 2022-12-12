d0d074c2e3c51d76e59da189b079e0cc919cb9b2
Amanda <amandagayotto@hotmail.com>
2022-12-11 21:03:43 -0300

    feat: popula tabelas Users e Posts

---
+++ Migrations/20221211232845_PopulaUsers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tryitter.Migrations
+{
+    public partial class PopulaUsers : Migration
+    {
+        protected override void Up(MigrationBuilder mB)
+        {
+            mB.Sql("Insert into Users(Nome, Email, Senha, Modulo, Status) Values('Amanda', 'amandagayotto@hotmail.com', 'minhasenha', 'c#', 'finalizando projeto aceleração')");
+            mB.Sql("Insert into Users(Nome, Email, Senha, Modulo, Status) Values('Marcelo', 'marcelodc@hotmail.com', 'senha123', 'python', 'finalizando módulo')");
+            mB.Sql("Insert into Users(Nome, Email, Senha, Modulo, Status) Values('Laura', 'laurasouza@hotmail.com', 'senhasecreta', 'css', 'finalizando projeto com estilização')");
+        }
+
+        protected override void Down(MigrationBuilder mB)
+        {
+            mB.Sql("Delete from Users");
+        }
+    }
+}
---
+++ Migrations/20221211234847_PopulaPosts.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tryitter.Migrations
+{
+    public partial class PopulaPosts : Migration
+    {
+        protected override void Up(MigrationBuilder mB)
+        {
+            mB.Sql("Insert into Posts(Texto, ImagemUrl, UserId) Values('Como se preparar para o mercado de trabalho', 'coffeimage.jpeg', 1)");
+            mB.Sql("Insert into Posts(Texto, ImagemUrl, UserId) Values('Ferramentas para criar protótipos de projetos web', 'fotofigma.jpeg', 2)");
+            mB.Sql("Insert into Posts(Texto, ImagemUrl, UserId) Values('A vida real de um dev', 'computersimage.png', 3)");
+        }
+
+        protected override void Down(MigrationBuilder mB)
+        {
+            mB.Sql("Delete from Posts");
+        }
+    }
+}
